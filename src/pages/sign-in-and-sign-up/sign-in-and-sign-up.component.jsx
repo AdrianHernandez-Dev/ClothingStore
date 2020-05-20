fd843e31a1b6688a5f53fa36338f94e950ae686c
AdrianHernandez-Dev <a.a.hernandez00@gmail.com>
2020-05-19 21:16:13 -0600

    Added the sign in component and styled the button

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../sign-in-and-sign-up/sign-in-and-sign-up.style.scss'
+import SignIn from '../../components/sing-in/sign-in.component'
+
+function SignInAndSignUpPage () {
+  return <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+}
+
+export default SignInAndSignUpPage
